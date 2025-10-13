7c663ccbf3960829cbcda3978d1f346480c32ea4
Gregor Decristoforo <gregor.decristoforo@gmail.com>
2025-10-13 12:46:18 +0200

    Change random seed from 42 to 420

--- generate_data.py
+++ generate_data.py
@@ -58,7 +58,7 @@ def main(num_samples, training_data, test_data):
     Program that generates a set of training and test samples for a non-linear classification task.
     """
 
-    rng = np.random.default_rng(seed=42)
+    rng = np.random.default_rng(seed=420)
 
     for output_file in [training_data, test_data]:
         data = generate_data(rng, num_samples)
