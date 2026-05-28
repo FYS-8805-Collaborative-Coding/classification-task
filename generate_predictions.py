24f21fab28fdadd8c55ec8572d3092df44c922bd
Gregor Decristoforo <gregor.decristoforo@gmail.com>
2026-05-28 09:54:40 +0200

    add first pytest tests

--- generate_predictions.py
+++ generate_predictions.py
@@ -34,6 +34,21 @@ def nearest_neighbor_predictor(df_train, df_test, num_neighbors):
     return predictions
 
 
+def fahrenheit_to_celsius(temp_f):
+    """Converts temperature in Fahrenheit
+    to Celsius.
+    """
+    temp_c = (temp_f - 32.0) * (5.0/9.0)
+    return temp_c
+
+
+# this is the test function
+def test_fahrenheit_to_celsius():
+    temp_c = fahrenheit_to_celsius(temp_f=100.0)
+    expected_result = 37.777777
+    # assert raises an error if the condition is not met
+    assert abs(temp_c - expected_result) < 1.0e-6
+
 @click.command()
 @click.option(
     "--num-neighbors",
